9ccf1a79009f635de2e386183dd49cc45e9535ca
Clay Draper <clayddraper@gmail.com>
2020-03-26 15:41:30 -0500

    started api, connected to mysql, implemented post method for spaces

---
+++ backend/spaceRatingApp/src/main/java/com/spaceRatingApp/SpaceRatingAppApplication.java
@@ -0,0 +1,13 @@
+package com.spaceRatingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceRatingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceRatingAppApplication.class, args);
+	}
+
+}
---
+++ backend/spaceRatingApp/src/main/java/com/spaceRatingApp/entity/SpaceEntity.java
@@ -0,0 +1,90 @@
+package com.spaceRatingApp.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name="spaces")
+public class SpaceEntity {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(nullable=false)
+	private String externalId;
+	
+	@Column(nullable=false)
+	private String name;
+	
+	@Column(nullable=false)
+	private String city;
+	
+	@Column(nullable=false)
+	private String state;
+	
+	@Column(nullable=false)
+	private String description;
+	
+	@Column(nullable=false)
+	private String photo;
+
+//	 getters and setters
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getExternalId() {
+		return externalId;
+	}
+
+	public void setExternalId(String externalId) {
+		this.externalId = externalId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+}
